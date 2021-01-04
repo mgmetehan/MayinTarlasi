8e6c111b66f765ff74434ea020ada1d7cad3fb31
Metehan Gültekin <41691766+mgmetehan@users.noreply.github.com>
2021-01-05 00:40:30 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,22 @@
+package game;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		int satir, sutun;
+
+		System.out.println("Mayin Tarlasi Oyununa Hos Geldiniz!");
+		System.out.println("Kac kaclik alan yaratilisin");
+		System.out.print("Satir: ");
+		satir = scan.nextInt();
+		System.out.print("Sutun: ");
+		sutun = scan.nextInt();
+
+		System.out.println("-1->Mayin\t-2->Mayin Yok\t 9->Önceden Ziyaret Edilen Nokta");
+		Oyun gm = new Oyun(satir, sutun);
+		gm.start();
+	}
+}
